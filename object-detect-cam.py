3411d18c31d41ff506b6eb74b8ecb90981f0b735
Adelene <47531026+AdeleneLSW15@users.noreply.github.com>
2024-04-13 15:01:42 +0100

    object-detection-file

---
+++ object-detect-cam.py
@@ -0,0 +1,21 @@
+
+"""
+Use yolov8 to perform object detection. 
+
+Make it -> detect a bottle and send it over to serial (shoot a bottle function)
+"""
+#need to install ultralytics package - pip install ultralytics.
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
+import cv2
+
+# Load a model
+#model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+
+results = model.predict(source="0", show=True, conf=0.4, save=True)
+print (results)
+
+#condition: do X when detect one or two person?
