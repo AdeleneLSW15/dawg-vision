14c25cb075197312a3280d6a99bb8189e89ad1a0
Adelene <47531026+AdeleneLSW15@users.noreply.github.com>
2024-04-13 17:59:08 +0100

    cuda-object-detect

--- object-detect-cam.py
+++ object-detect-cam.py
@@ -8,19 +8,26 @@ Make it -> detect a bottle and send it over to serial (shoot a bottle function)
 from ultralytics import YOLO
 from ultralytics.models.yolo.detect.predict import DetectionPredictor
 import cv2
+import torch
+import torch.onnx
+import torchvision.models as models
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load a model
 #model = YOLO("yolov8n.yaml")  # build a new model from scratch
-model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
-
+model = YOLO("yolov8s.pt", device = device)  # load a pretrained model (recommended for training)
+#model.to('cuda')
 #For Nvidia Jetson Nano: TensorFlow RT for better inference time
 model.export(format='engine')
 #load to TensorRT mode
-trt_model = YOLO('yolov8s.engine')
+
+
 
 
 # Use the model - running inference
-results = trt_model.predict(source="0", show=True, conf=0.4, save=True)
-print (results)
+#results = trt_model.predict(source="0", show=True, stream = True, conf=0.4, save=True)
+#print (results)
+#results = model.predict(source='video source', task='detect', stream=True, save=False, verbose=False, conf = 0.5
 
 #condition: do X when detect one or two person?
